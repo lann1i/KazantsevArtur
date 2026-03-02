2fe856acc075053845b6eb89de421b036c002a65
lanni <a-kazantsev8@mail.ru>
2026-03-02 11:48:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kazantsevartur/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.kazantsevartur;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Log.i("Унбо-05-24", "Информация");
+        Button myButton = findViewById(R.id.my_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Действие при нажатии на кнопку, например:
+                Toast.makeText(getApplicationContext(), "Кнопка нажата",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.e("Унбо-05-24", "Ошибка");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d("Унбо-05-24", "Проверка");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.v("Унбо-05-24", "Пауза");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.v("Унбо-05-24", "Стоп");
+    }
+
+    protected void onDestroy() {
+        Log.v("Унбо-05-24", "Destroy");
+        super.onDestroy();
+    }
+
+    public void onNextActivity(View view) {
+        EditText nameText = findViewById(R.id.editTextText2);
+        EditText groupText = findViewById(R.id.editTextText);
+        EditText ageText = findViewById(R.id.editTextNumber);
+        EditText scoreText = findViewById(R.id.editTextNumber2);
+        String name = nameText.getText().toString();
+        String group = groupText.getText().toString();
+        int age = Integer.parseInt(ageText.getText().toString());
+        int score = Integer.parseInt(scoreText.getText().toString());
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("name", name);
+        intent.putExtra("group", group);
+        intent.putExtra("age", age);
+        intent.putExtra("score", score);
+        startActivity(intent);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/kazantsevartur/MainActivity2.java
@@ -0,0 +1,41 @@
+package com.example.kazantsevartur;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Bundle arguments = getIntent().getExtras();
+        if(arguments != null) {
+            String name = arguments.get("name").toString();
+            String group = arguments.get("group").toString();
+            int age = arguments.getInt("age");
+            int score = arguments.getInt("score");
+            TextView messageText = findViewById(R.id.textView2);
+            messageText.setText("Name " + name + "\nGroup " + group + "\nAge " + age + "\nScore " + score);
+        }
+    }
+    public void onNextActivity1(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
